5fb569fb545ffc423bdd298ad7fa133e5dfbbb59
Bodigrim <andrew.lelechenko@gmail.com>
2015-08-02 21:13:14 +0300

    Convert tabs to spaces

--- src/Data/FastDigits.hs
+++ src/Data/FastDigits.hs
@@ -15,13 +15,13 @@ For small bases and long numbers it may be up to 30 times faster.
 {-# LANGUAGE MagicHash     #-}
 {-# LANGUAGE UnboxedTuples #-}
 
-{-# OPTIONS_GHC -fno-warn-type-defaults	 #-}
-{-# OPTIONS_GHC -O2	                     #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults  #-}
+{-# OPTIONS_GHC -O2                      #-}
 
 module Data.FastDigits
-	( digits
-	, undigits
-	) where
+  ( digits
+  , undigits
+  ) where
 
 import Control.Arrow
 import GHC.Exts
@@ -35,38 +35,38 @@ fi = fromInteger
 
 digitsInteger :: Integer -> Integer -> [Int]
 digitsInteger base = f
-	where
-		f 0 = []
-		f n = let (# q, r #) = n `quotRemInteger` base in fi r : f q
+  where
+    f 0 = []
+    f n = let (# q, r #) = n `quotRemInteger` base in fi r : f q
 
 digitsInt :: Int -> Int -> [Int]
 digitsInt (I# base) (I# m) = f m
-	where
-		f :: Int# -> [Int]
-		f 0# = []
-		f n = let (# q, r #) = n `quotRemInt#` base in (I# r) : f q
+  where
+    f :: Int# -> [Int]
+    f 0# = []
+    f n = let (# q, r #) = n `quotRemInt#` base in (I# r) : f q
 
 
 digitsInteger' :: Int -> Int -> Integer -> Integer -> [Int]
 digitsInteger' power base poweredBase = fst . f
-	where
-		f :: Integer -> ([Int], Int)
-		f n = case n `quotRem` poweredBase of
-			(0, _) -> (id &&& length) (digitsInt base $ fi n)
-			(q, r) -> (fr ++ replicate (power - lr) 0 ++ fq, lq)
-								where
-									(fq, lq) = f q
-									(fr, lr) = f r
+  where
+    f :: Integer -> ([Int], Int)
+    f n = case n `quotRem` poweredBase of
+      (0, _) -> (id &&& length) (digitsInt base $ fi n)
+      (q, r) -> (fr ++ replicate (power - lr) 0 ++ fq, lq)
+                where
+                  (fq, lq) = f q
+                  (fr, lr) = f r
 
 selectPower :: Int -> (Int, Int)
 selectPower base = if poweredBase > 0
-		then (power, poweredBase)
-		else (power - 1, base ^ (power - 1))
-	where
-		power :: Int
-		power = floor $ logBase (fi $ ti base) (fi $ ti $ (maxBound :: Int))
-		poweredBase :: Int
-		poweredBase = base ^ power
+    then (power, poweredBase)
+    else (power - 1, base ^ (power - 1))
+  where
+    power :: Int
+    power = floor $ logBase (fi $ ti base) (fi $ ti $ (maxBound :: Int))
+    poweredBase :: Int
+    poweredBase = base ^ power
 
 -- | Return digits of a non-negative integer in reverse order. E. g.,
 --
@@ -75,18 +75,18 @@ selectPower base = if poweredBase > 0
 --
 -- Throw an error if number is negative or base is below 2.
 digits
-	:: Int     -- ^ The base to use
-	-> Integer -- ^ The integer to convert
-	-> [Int]
+  :: Int     -- ^ The base to use
+  -> Integer -- ^ The integer to convert
+  -> [Int]
 digits base n
-	| base < 2  = error "Base must be > 1"
-	| n < 0     = error "Number must be non-negative"
-	| n < ti (maxBound :: Int) = digitsInt base (fi n)
-	| otherwise = if power == 1
-									then digitsInteger (ti base) n
-									else digitsInteger' power base (ti poweredBase) n
-	where
-		(power, poweredBase) = selectPower base
+  | base < 2  = error "Base must be > 1"
+  | n < 0     = error "Number must be non-negative"
+  | n < ti (maxBound :: Int) = digitsInt base (fi n)
+  | otherwise = if power == 1
+                  then digitsInteger (ti base) n
+                  else digitsInteger' power base (ti poweredBase) n
+  where
+    (power, poweredBase) = selectPower base
 
 -- | Return an integer, built from given digits in reverse order.
 --   Condition 0 <= digit < base is not checked.
@@ -95,7 +95,7 @@ undigits :: (Integral a, Integral b)
          -> [b]     -- ^ The list of digits to convert
          -> Integer
 undigits base' = foldr (\d acc -> acc * base + ti d) 0
-	where
-		base = ti base'
+  where
+    base = ti base'
 {-# SPECIALIZE undigits :: Int -> [Int] -> Integer #-}
 {-# SPECIALIZE undigits :: Integer -> [Integer] -> Integer #-}
