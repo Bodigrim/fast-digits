281a99f053d1be8e0fe54b982a8101fab2a4e5e5
Bodigrim <andrew.lelechenko@gmail.com>
2016-01-01 20:01:57 +0200

    Add comments

--- src/Data/FastDigits.hs
+++ src/Data/FastDigits.hs
@@ -34,14 +34,12 @@ ti = toInteger
 fi :: Num a => Integer -> a
 fi = fromInteger
 
-
+-- | First argument should be below maxBound :: Int.
 digitsInteger :: Integer -> Integer -> [Int]
 digitsInteger base = f
   where
     f 0 = []
     f n = let (# q, r #) = n `quotRemInteger` base in fi r : f q
-
--- Use Word instead of Int!
 
 digitsInt :: Int# -> Int -> [Int]
 digitsInt base (I# m) = f m
@@ -50,8 +48,10 @@ digitsInt base (I# m) = f m
     f 0# = []
     f n = let (# q, r #) = n `quotRemInt#` base in I# r : f q
 
+-- | For a given base and expected length of list of digits
+--   return the list of digits and padding till expected length.
 digitsIntL :: Int# -> Int# -> Int -> (# [Int], Int# #)
-digitsIntL power base (I# m) = f m
+digitsIntL base power (I# m) = f m
   where
     f :: Int# -> (# [Int], Int# #)
     f 0# = (# [], power #)
@@ -60,29 +60,32 @@ digitsIntL power base (I# m) = f m
         (# q, r #) = n `quotRemInt#` base
         (# fq, lq #) = f q
 
+-- | For a given base, power and precalculated base^power
+--   take an integer and return the list of its digits.
 digitsInteger' :: Int# -> Int# -> Integer -> Integer -> [Int]
-digitsInteger' power base poweredBase = f
+digitsInteger' base power poweredBase = f
   where
     f :: Integer -> [Int]
     f n = case  n `quotRemInteger` poweredBase of
       (# 0, _ #) -> digitsInt base (fi n)
       (# q, r #) -> fr ++ replicate (I# lr) 0 ++ f q
                     where
-                      (# fr, lr #) = digitsIntL power base (fi r)
+                      (# fr, lr #) = digitsIntL base power (fi r)
 
 -- | Take an integer base and return (pow, base^pow),
 --   where base^pow <= maxBound and pow is as large as possible.
 selectPower :: Int# -> (# Int#, Int# #)
-selectPower base = if I# poweredBase' > 0
-    then (# power', poweredBase' #)
-    else (# power,  poweredBase  #)
+selectPower base = -- if I# poweredBase' > 0
+    --then
+    (# power', poweredBase' #)
+    --else (# power,  poweredBase  #)
   where
     !(I# maxB) = maxBound
 
     power = double2Int# (logDouble# (int2Double# base) /## logDouble# (int2Double# maxB))
     power' = power +# 1#
 
-    !(I# poweredBase) = (I# base) ^ (I# power)
+    -- !(I# poweredBase) = (I# base) ^ (I# power)
     !(I# poweredBase') = (I# base) ^ (I# power')
 
 -- | Return digits of a non-negative integer in reverse order. E. g.,
@@ -101,7 +104,7 @@ digits base@(I# base') n
   | n < ti (maxBound :: Int) = digitsInt base' (fi n)
   | otherwise = if I# power == 1
                   then digitsInteger (ti base) n
-                  else digitsInteger' power base' (ti $ I# poweredBase) n
+                  else digitsInteger' base' power (ti $ I# poweredBase) n
   where
     (# power, poweredBase #) = selectPower base'
 
@@ -114,5 +117,6 @@ undigits :: (Integral a, Integral b)
 undigits base' = foldr (\d acc -> acc * base + ti d) 0
   where
     base = ti base'
+{-# SPECIALIZE undigits :: Word -> [Word] -> Integer #-}
 {-# SPECIALIZE undigits :: Int -> [Int] -> Integer #-}
 {-# SPECIALIZE undigits :: Integer -> [Integer] -> Integer #-}
