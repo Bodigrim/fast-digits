d550443e8fea7c23f17588f1ab5c25fe0af0138c
Bodigrim <andrew.lelechenko@gmail.com>
2021-03-02 00:43:12 +0000

    Rework benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,15 +1,18 @@
-module Main where
+{-# LANGUAGE CPP #-}
 
-import Gauge.Main
+module Main (main) where
 
-import qualified Data.Digits as D
+#ifdef MIN_VERSION_digits
+import qualified Data.Digits as D (digitsRev)
+#endif
+import Data.FastDigits (digits, undigits)
+import Data.List (foldl')
+import Test.Tasty.Bench (Benchmark, defaultMain, bench, bgroup, nf)
 
-import Data.FastDigits
-
+#ifdef MIN_VERSION_digits
 digitsD :: Int -> Integer -> [Int]
 digitsD base n = map fromInteger $ D.digitsRev (toInteger base) n
-
-
+#endif
 
 intNs :: Int -> Int -> [Int]
 intNs from len = [from, step .. maxBound]
@@ -22,32 +25,45 @@ integerN from len = undigits (maxBound :: Int) (intNs from len)
 integerNs :: Int -> Int -> [Integer]
 integerNs llen len = map (\i -> integerN i len) [1..llen]
 
-main = defaultMain [
-  bgroup "shortInt"  [ bench "FastDigits  base 2   " $ nf (map $ digits  2      . toInteger) (intNs 1 10000)
-                     , bench "Data.Digits base 2   " $ nf (map $ digitsD 2      . toInteger) (intNs 1 10000)
-                     , bench "FastDigits  base 10  " $ nf (map $ digits  10     . toInteger) (intNs 1 10000)
-                     , bench "Data.Digits base 10  " $ nf (map $ digitsD 10     . toInteger) (intNs 1 10000)
-                     , bench "FastDigits  base 10^5" $ nf (map $ digits  (10^5) . toInteger) (intNs 1 10000)
-                     , bench "Data.Digits base 10^5" $ nf (map $ digitsD (10^5) . toInteger) (intNs 1 10000)
-                     , bench "FastDigits  base 10^9" $ nf (map $ digits  (10^9) . toInteger) (intNs 1 10000)
-                     , bench "Data.Digits base 10^9" $ nf (map $ digitsD (10^9) . toInteger) (intNs 1 10000)
-                     ],
-  bgroup "mediumInt" [ bench "FastDigits  base 2   " $  nf (map $ digits  2     ) (integerNs 100 10)
-                     , bench "Data.Digits base 2   " $  nf (map $ digitsD 2     ) (integerNs 100 10)
-                     , bench "FastDigits  base 10  " $  nf (map $ digits  10    ) (integerNs 100 10)
-                     , bench "Data.Digits base 10  " $  nf (map $ digitsD 10    ) (integerNs 100 10)
-                     , bench "FastDigits  base 10^5" $  nf (map $ digits  (10^5)) (integerNs 100 10)
-                     , bench "Data.Digits base 10^5" $  nf (map $ digitsD (10^5)) (integerNs 100 10)
-                     , bench "FastDigits  base 10^9" $  nf (map $ digits  (10^9)) (integerNs 100 10)
-                     , bench "Data.Digits base 10^9" $  nf (map $ digitsD (10^9)) (integerNs 100 10)
-                     ],
-  bgroup "longInt"   [ bench "FastDigits  base 2   " $  nf (digits  2     ) (integerN 1 1000)
-                     , bench "Data.Digits base 2   " $  nf (digitsD 2     ) (integerN 1 1000)
-                     , bench "FastDigits  base 10  " $  nf (digits  10    ) (integerN 1 1000)
-                     , bench "Data.Digits base 10  " $  nf (digitsD 10    ) (integerN 1 1000)
-                     , bench "FastDigits  base 10^5" $  nf (digits  (10^5)) (integerN 1 1000)
-                     , bench "Data.Digits base 10^5" $  nf (digitsD (10^5)) (integerN 1 1000)
-                     , bench "FastDigits  base 10^9" $  nf (digits  (10^9)) (integerN 1 1000)
-                     , bench "Data.Digits base 10^9" $  nf (digitsD (10^9)) (integerN 1 1000)
-                     ]
+benchShort :: String -> (Integer -> [Int]) -> Benchmark
+benchShort name f = bench name $ flip nf 10000 $
+  \len -> foldl' (+) 0 $ concatMap (f . toInteger)
+    [1 :: Int, maxBound `quot` len .. maxBound]
+{-# INLINE benchShort #-}
+
+benchMedium :: String -> (Integer -> [Int]) -> Benchmark
+benchMedium name f = bench name $ flip nf 100 $
+  \len -> foldl' (+) 0 $ concatMap (f . flip integerN 10) [1 :: Int .. len]
+{-# INLINE benchMedium #-}
+
+benchLong :: String -> (Integer -> [Int]) -> Benchmark
+benchLong name f = bench name $ flip nf 1000 $
+  \len -> foldl' (+) 0 $ f $ integerN 1 len
+{-# INLINE benchLong #-}
+
+benchBase :: (String -> (Integer -> [Int]) -> Benchmark) -> Int -> Benchmark
+#ifdef MIN_VERSION_digits
+benchBase b base = bgroup (show base)
+  [ b "FastDigits" (digits base)
+  , b "Digits" (digitsD base)
+  ]
+#else
+benchBase b base = b (show base) (digits base)
+#endif
+{-# INLINE benchBase #-}
+
+benchSmth :: String -> (String -> (Integer -> [Int]) -> Benchmark) -> Benchmark
+benchSmth name b = bgroup name $
+  [ benchBase b 2
+  , benchBase b 10
+  , benchBase b (10^5)
+  , benchBase b (10^9)
+  ]
+{-# INLINE benchSmth #-}
+
+main :: IO ()
+main = defaultMain
+  [ benchSmth "short"  benchShort
+  , benchSmth "medium" benchMedium
+  , benchSmth "long"   benchLong
   ]
